246baadf7b64871da69d7b16acdf02ee0e614eba
Max Linke <maxlinke@users.noreply.github.com>
2018-08-12 23:39:48 +0200

    Attempt at CelShading
    > Just an attempt, as in getting the delta-depth and deciding whether to color a pixel black based on that...
    > Also added the freecam from my other testproject

---
+++ Assets/Freecam/CameraMoveScript.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEditor;
+
+public class CameraMoveScript : MonoBehaviour {
+
+	[Header("Input")]
+	public KeyCode keyForwards;
+	public KeyCode keyBackwards;
+	public KeyCode keyLeft;
+	public KeyCode keyRight;
+	public KeyCode keyUp;
+	public KeyCode keyDown;
+	public KeyCode keyFast;
+	public KeyCode keySlow;
+	public KeyCode keyLightToggle;
+	public KeyCode keyReset;
+	[Header("Params")]
+	public Camera cam;
+	public Light torch;
+	public float normalSpeed;
+	public float fastSpeed;
+	public float slowSpeed;
+	public float mouseSensitivity;
+
+	private bool mouseLookEnabled;
+
+	private Vector3 startPos;
+	private Quaternion startRot;
+	private Vector3 camStartRot;
+
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		mouseLookEnabled = true;
+		if(torch != null) torch.gameObject.SetActive(false);
+		startPos = transform.position;
+		startRot = transform.rotation;
+		camStartRot = cam.transform.localEulerAngles;
+	}
+	
+	void Update () {
+		if(mouseLookEnabled) MouseLook();
+		Movement();
+		if(Input.GetKeyDown(keyLightToggle) && (torch != null)) torch.gameObject.SetActive(!torch.gameObject.activeSelf);
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			//if(!mouseLookEnabled) UnityEditor.EditorApplication.isPlaying = false;
+			//else mouseLookEnabled = false;
+			Cursor.lockState = CursorLockMode.None;
+			mouseLookEnabled = false;
+		}
+		if(Input.GetKeyDown(KeyCode.Mouse0)){
+			Cursor.lockState = CursorLockMode.Locked;
+			mouseLookEnabled = true;
+		}
+		if(Input.GetKeyDown(keyReset)){
+			transform.position = startPos;
+			transform.rotation = startRot;
+			cam.transform.localEulerAngles = camStartRot;
+		}
+	}
+
+	private void MouseLook(){
+		Vector2 mouse = GetMouseMovement() * mouseSensitivity;
+		transform.Rotate(new Vector3(0f, mouse.x, 0f));
+		cam.transform.Rotate(new Vector3(mouse.y, 0f, 0f) * (-1f));
+	}
+
+	private Vector2 GetMouseMovement(){
+		float mouseX = Input.GetAxisRaw("Mouse X");
+		float mouseY = Input.GetAxisRaw("Mouse Y");
+		return new Vector2(mouseX, mouseY);
+	}
+
+	private void Movement(){
+		float moveSpeed = GetMoveSpeed();
+		Vector3 inputVector = GetInputVector();
+		Vector3 moveVector = cam.transform.TransformDirection(inputVector) * moveSpeed;
+		transform.position += moveVector * Time.deltaTime;
+	}
+
+	private float GetMoveSpeed(){
+		if(Input.GetKey(keyFast)) return fastSpeed;
+		if(Input.GetKey(keySlow)) return slowSpeed;
+		return normalSpeed;
+	}
+
+	private Vector3 GetInputVector(){
+		Vector3 output = Vector3.zero;
+		if(Input.GetKey(keyForwards)) output += Vector3.forward;
+		if(Input.GetKey(keyBackwards)) output += Vector3.back;
+		if(Input.GetKey(keyLeft)) output += Vector3.left;
+		if(Input.GetKey(keyRight)) output += Vector3.right;
+		if(Input.GetKey(keyUp)) output += Vector3.up;
+		if(Input.GetKey(keyDown)) output += Vector3.down;
+		return output;
+	}
+}
---
+++ Assets/Shaders/Image Effects/CelShading.shader
@@ -0,0 +1,96 @@
+﻿Shader "Custom/Image Effects/CelShading"{
+
+	//TODO stuff more based on depth... like line weight (radius) both on "absolute" distance and delta distance
+	//also variable threshold i think...
+
+	Properties{
+		[HideInInspector] _MainTex ("Texture", 2D) = "white" {}
+		_Threshold ("Threshold", Range(0,1)) = 0.1
+		_Radius ("Radius", Range(0,10)) = 2.0
+	}
+
+	SubShader{
+
+		Cull Off
+		ZWrite Off
+		ZTest Always
+
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass{
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			half4 _MainTex_TexelSize;
+			sampler2D _CameraDepthTexture;
+
+			float _Threshold;
+			float _Radius;
+
+			struct appdata{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f{
+				float4 pos : SV_POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			float deltaDepth (float2 uv, float radius) {
+				float centerDepth = UNITY_SAMPLE_DEPTH(tex2D(_CameraDepthTexture, uv));
+
+				float maxDelta = 0;
+				for (int i=0; i<4; i++) {
+					float j = i * UNITY_TWO_PI / 4.0;
+					float2 uvOffset = float2(cos(j), sin(j));
+					uvOffset *= radius;
+					uvOffset /= _ScreenParams.xy;
+					float newDepth = UNITY_SAMPLE_DEPTH(tex2D(_CameraDepthTexture, uv + uvOffset));
+					float delta = abs(centerDepth - newDepth);
+					maxDelta = max(maxDelta, delta);
+				}
+				return maxDelta;
+
+//				float ix = 1.0 / _ScreenParams.x;
+//				float iy = 1.0 / _ScreenParams.y;
+//				float maxDelta = 0;
+//				maxDelta = max(maxDelta, abs(centerDepth - UNITY_SAMPLE_DEPTH(tex2D(_CameraDepthTexture, uv + float2(0, iy)))));
+//				maxDelta = max(maxDelta, abs(centerDepth - UNITY_SAMPLE_DEPTH(tex2D(_CameraDepthTexture, uv - float2(0, iy)))));
+//				maxDelta = max(maxDelta, abs(centerDepth - UNITY_SAMPLE_DEPTH(tex2D(_CameraDepthTexture, uv + float2(ix, 0)))));
+//				maxDelta = max(maxDelta, abs(centerDepth - UNITY_SAMPLE_DEPTH(tex2D(_CameraDepthTexture, uv - float2(ix, 0)))));
+//				return maxDelta;
+
+//				return centerDepth;
+			}
+			
+			v2f vert (appdata v){
+				v2f o;
+				o.pos = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				#if UNITY_UV_STARTS_AT_TOP
+					if(_MainTex_TexelSize.y < 0) o.uv.y = 1 - o.uv.y;	//does this work? i dont know... i just copy-pasted code :P
+				#endif
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target{
+//				float rawDepth = UNITY_SAMPLE_DEPTH(tex2D(_CameraDepthTexture, i.uv));
+				fixed4 tex = tex2D(_MainTex, i.uv);
+				float s = step(deltaDepth(i.uv, _Radius), _Threshold);
+				fixed4 col = (s * tex) + ((1-s) * fixed4(0,0,0,0));
+//				fixed4 col = deltaDepth(i.uv, _Radius);
+//				fixed4 col = rawDepth;
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
