83b0adf888d57ab58b3f486987e59759258edbee
Max Linke <maxlinke@users.noreply.github.com>
2019-05-12 18:50:47 +0200

    fog for deferred

---
+++ Assets/Shaders/Image Effects/FogImageEffect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FogImageEffect : MonoBehaviour {
+
+    [SerializeField] Camera cam;
+    [SerializeField] Shader fogEffect;
+
+    Material fogEffectMaterial;
+
+    [ContextMenu("Log rendering paths")]
+    void LogRendeingPaths () {
+        Debug.Log("Set: " + cam.renderingPath.ToString() + "\nActual: " + cam.actualRenderingPath.ToString());
+    }
+
+    void Reset () {
+        cam = GetComponent<Camera>();
+    }
+
+    [ImageEffectOpaque]
+    void OnRenderImage (RenderTexture src, RenderTexture dst) {
+        if(fogEffectMaterial == null){
+            fogEffectMaterial = new Material(fogEffect);
+        }
+        var renderPath = cam.actualRenderingPath;
+        if(renderPath != RenderingPath.DeferredShading){
+            Debug.LogWarning("This effect is meant to apply fog when using deferred shading. Currently using " + renderPath.ToString() + "!");
+            Graphics.Blit(src, dst);
+        }else{
+            if(cam.depthTextureMode == DepthTextureMode.None){
+                cam.depthTextureMode = DepthTextureMode.Depth;
+            }
+            Graphics.Blit(src, dst, fogEffectMaterial);
+        }
+    }
+
+}
