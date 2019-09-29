3f7d4fd017ed04a10af1dca11c697f00d5384ad6
Max Linke <maxlinke@users.noreply.github.com>
2019-09-29 20:34:01 +0200

    manual skyboxes and purposefully not clearing an area's rgb-buffer

---
+++ Assets/Scripts/ManualSkybox.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualSkybox : MonoBehaviour {
+
+	void OnWillRenderObject () {
+        transform.position = Camera.current.transform.position;
+        transform.localScale = Vector3.one * Camera.current.farClipPlane;
+    }
+
+}
---
+++ Assets/Shaders/Experimental/X_Holdout.shader
@@ -0,0 +1,18 @@
+﻿Shader "Custom/Experimental/X_Holdout" {
+
+	Properties { }
+
+	SubShader {
+
+		Tags { "RenderType"="Opaque" "Queue" = "Background-1" } //bg+1 to "clear to skybox"
+		LOD 100
+
+		Pass {
+
+			ColorMask 0
+            ZTest Always
+            ZWrite On
+
+		}
+	}
+}
