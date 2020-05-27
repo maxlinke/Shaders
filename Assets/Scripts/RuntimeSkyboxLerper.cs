67c17b69a12237999a3b1923e7b69d92c8a8d3b4
Max Linke <maxlinke@users.noreply.github.com>
2020-05-27 13:24:40 +0200

    added a script to enable runtime manipulation of the skybox lerp amount via timeline
    > doesn't matter in 5.6 but for later versions...
    > because timeline can only manipulate scene objects? idk...

---
+++ Assets/Scripts/RuntimeSkyboxLerper.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class RuntimeSkyboxLerper : MonoBehaviour {
+
+    [Range(0, 1)] public float lerpAmount;
+
+    int lerpPropID;
+    Material skyboxMat;
+
+	void Awake () {
+        lerpPropID = Shader.PropertyToID("_LerpAmount");
+        if(RenderSettings.skybox.HasProperty(lerpPropID)){
+		    skyboxMat = Instantiate(RenderSettings.skybox);
+            RenderSettings.skybox = skyboxMat;
+        }else{
+            skyboxMat = null;
+            Debug.LogError("Skybox Material doesn't seem to be lerpable!");
+        }
+	}
+	
+	void Update () {
+        if(skyboxMat == null){
+            return;
+        }
+		if(skyboxMat.GetFloat(lerpPropID) != lerpAmount){
+            skyboxMat.SetFloat(lerpPropID, lerpAmount);
+            DynamicGI.UpdateEnvironment();
+        }
+	}
+}
